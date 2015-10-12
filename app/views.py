6cd733975c79388d20f3760c1015b68ea0042cfe
kcamrine <kc.amrine@gmail.com>
2015-10-12 01:21:36 -0700

    clean paths

--- app/views.py
+++ app/views.py
@@ -14,8 +14,12 @@ import pickle
 import random
 import string
 import re
+import cv2 
+import sys
+import numpy as np
 
 sys_args = pickle.load( open("api_config.p", "rb") ) #load in private settings
+savedata = pickle.load( open( "dirpaths_config.p", "rb" ) )
 
 db = psycopg2.connect(user=sys_args['user'], database=sys_args['database'])
 
@@ -27,6 +31,16 @@ app.config['UPLOAD_RAW_DEST'] = UPLOAD_FOLDER_RAW
 app.config['UPLOAD_EDIT_DEST'] = UPLOAD_FOLDER_EDIT
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER_RAW
+
+global model_SF, model_LA, model_S
+
+model_SF = cv2.createEigenFaceRecognizer(threshold=100000) ## NEW
+model_LA = cv2.createEigenFaceRecognizer(threshold=100000) ## NEW 
+model_S = cv2.createEigenFaceRecognizer(threshold=100000) ## NEW
+
+model_SF.load("app/eigenModel_SF.xml") ## NEW
+model_LA.load("app/eigenModel_LA.xml") ## NEW
+model_S.load("app/eigenModel_S.xml") ## NEW
 
 # check file is ok
 def allowed_file(filename):
@@ -48,6 +62,8 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 @app.route('/output',methods=['POST'])
 def report_match():
     file = request.files['file']
+    city = request.form.getlist('City')[0] #which model to use ##NEW
+    
     if file and allowed_file(file.filename):
         file.filename = str(id_generator(size = 12)) + '.jpg'
         filename = secure_filename(file.filename)
@@ -65,13 +81,26 @@ def report_match():
         return redirect(url_for('no_face_detected'))
     else:
     #match to database (meat 2)
-    #make these NOT hard-coded
-        matched_cat = match_petfinder.match_faces(image=human, 
-                                              threshold=100000, 
-                                              model_name="eigenModel.xml")
+    # Read the image we're looking for                                                                                 
+        sampleImage = cv2.imread(human, cv2.IMREAD_GRAYSCALE)
+        sampleImage = cv2.resize(sampleImage, (256,256))
+    # Look through the model and find the face it matches                                                              
+        if city == "SF":
+            [p_label, p_confidence] = model_SF.predict(sampleImage)
+            matched_cat = savedata['filelist_SF'][p_label]
+        elif city == "LA":
+            [p_label, p_confidence] = model_LA.epredict(sampleImage)
+            matched_cat = savedata['filelist_LA'][p_label]
+        elif city == "S":
+            [p_label, p_confidence] = model_S.predict(sampleImage)
+            matched_cat = savedata['filelist_S'][p_label]
+# replace with above
+#        matched_cat = match_petfinder.match_faces(image=human, 
+#                                              threshold=100000, 
+#                                              model_name="eigenModel.xml")
         #extract metadata
+        print "matched cat: " + matched_cat
         with db:
-            print "matched cat: " + matched_cat
             cur = db.cursor()
             sql_query = "SELECT a.id, a.name, a.picture, b.city FROM name_data_table a join contact_info_data_table b on a.id=b.id where b.id='" + matched_cat + "';"
             query_results = pd.read_sql_query(sql_query,db)
@@ -112,5 +141,5 @@ def no_face_detected():
             file.save(os.path.join(app.config['UPLOAD_RAW_DEST'],filename))
             print filename
             return redirect(url_for('report_match',filename=filename))
-    return render_template("re_enter.html") #this doesn't exist yet
+    return render_template("re_enter.html")
 
